c076a978c9a08cbcc15e7eea78904e6d9088749d
Caio <caioteixeirasouza@gmail.com>
2025-09-29 17:24:04 -0300

    alteraçõe

--- src/app/components/header-component/header-component.ts
+++ src/app/components/header-component/header-component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-header-component',
-  imports: [ MatCardModule ],
+  imports: [ MatCardModule, MatInputModule, MatFormFieldModule, FormsModule, MatIconModule, MatButtonModule ],
   templateUrl: './header-component.html',
   styleUrl: './header-component.css'
 })
---
+++ src/app/type.ts
@@ -0,0 +1,7 @@
+interface WEATHER_PREDICTION {
+    City:string,
+    Date:string,
+    Description:string,
+    MaxTemperature:number,
+    MinTemperature:number,
+}
