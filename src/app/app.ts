1cbd4624923aebd9bbac575ec4dd401c55d9c222
Caio <caioteixeirasouza@gmail.com>
2025-09-29 14:00:56 -0300

    removendo component

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,12 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MainComponent } from './components/main-component/main-component';
 import { HeaderComponent } from './components/header-component/header-component';
-import { ComponentBody } from './components/component-body/component-body';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MainComponent, HeaderComponent, ComponentBody],
+  imports: [RouterOutlet, MainComponent, HeaderComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/components/main-component/main-component.ts
+++ src/app/components/main-component/main-component.ts
@@ -2,11 +2,10 @@ import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { HeaderComponent } from '../header-component/header-component';
-import { ComponentBody } from '../component-body/component-body';
 
 @Component({
   selector: 'app-main-component',
-  imports: [ComponentBody , HeaderComponent, MatCardModule, MatButtonModule],
+  imports: [ HeaderComponent, MatCardModule, MatButtonModule],
   templateUrl: './main-component.html',
   styleUrl: './main-component.css'
 })
